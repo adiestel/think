463d9e1068fe27451bdb238b87bc758113636896
adiestel <867839+adiestel@users.noreply.github.com>
2023-11-15 02:06:27 -0600

    more experiments

--- src/app/api/think/route.ts
+++ src/app/api/think/route.ts
@@ -1,13 +1,9 @@
 export async function GET(request: Request) {
-    console.log('GET');
-    console.log(request);
     return Response.json({url:request.url})
   }
 
 
 export async function POST(request: Request) {
-    console.log('POST');
-    console.log(request);
     let data = await request.text()
     return Response.json({"text":data})
 }
